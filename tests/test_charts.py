625fa65962af701314882602565ce427054f905d
Babila <babila.lima30@gmail.com>
2019-03-10 20:25:58 -0400

    write tests for data_handler and chart_maker modules

---
+++ tests/test_charts.py
@@ -0,0 +1,15 @@
+from  configparser import ConfigParser
+import os
+import unittest
+
+
+class TestChartImageFileGenerator(unittest.TestCase):
+    def put_future_tests_here():
+        # test that first chart returns file string
+        # test that second chart retruns file string
+        # test that the files will be saved @ data/images
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,23 +1,61 @@
-import configparser.ConfigParser
+from  configparser import ConfigParser
 import os
 import unittest
 
+from data_handler import get_data
+import pandas as pd
+
+class TestConfigFileExistence(unittest.TestCase):
+    def setUp(self):
+        self.file = os.path.join(os.pardir, 'configuration', 'config.ini')
+        self.config = ConfigParser()
+        self.config.read(self.file)
+        self.section_names = ['api_key','api_secret','access_token',
+                            'token_secret','datadotworld']
+
+    def tearDown(self):
+        pass
+
+    def test_configfile_existence(self):
+        self.assertEqual(os.path.join(os.pardir, 'configuration', 'config.ini'),
+                        self.file)
+
+    def test_configfile_sections(self):
+        self.assertEqual(self.config.sections(), self.section_names)
+
+
 class TestDataRetrieval(unittest.TestCase):
+    def setUp(self):
+        self.file = os.path.join(os.pardir, 'configuration', 'config.ini')
+        self.config = ConfigParser()
+        self.config.read(self.file)
+        self.sections = self.config.sections()
+        self.raw_data = data_handler.get_data(self.config.get(section='datadotworld',
+                        option='key'), self.config.get(section='datadotworld',
+                        option='data_name'))
 
-    def test_upper(self):
-        self.assertEqual('test dataframe type'.upper(),'TEST DATAFRAME TYPE')
+    def tearDown():
+        pass
 
-        # test for existence of key, data_name variables for datadotworld
+    def test_get_data_function_return(self):
+        self.assertEqual(type(self.raw_data), type(pd.DataFrame))
 
-        # test that clean_data function returns target number of columns
+    def test_clean_data_function_return(self):
+        self.assertEqual(type(data_handler.clean_data(self.raw_data)),
+                        type(pd.DataFrame))
 
-        # test that the dataframe function returns the target column names
+    # test that dataframe function returns certain basic columns
 
-        # test that the config.ini file exists
+class TestChartImageFileGenerator(unittest.TestCase):
+    def put_future_tests_here():
+        # test that first chart returns file string
+        # test that second chart retruns file string
+        # test that the files will be saved @ data/images
+        pass
 
-        # test that teh config.ini file has the required sections for .get call
 
-        # test that the data function call has all required arguments
+
+
 
 if __name__ == '__main__':
     unittest.main()
