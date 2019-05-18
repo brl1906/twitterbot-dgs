94d7c41b221dc68bca9861e2e45f60a5b9fe5352
Babila <babila.lima30@gmail.com>
2019-05-18 11:40:59 -0400

    move logging to logs directory

--- program/app.py
+++ program/app.py
@@ -1,10 +1,17 @@
 import os, sys
+import shutil
 from datetime import datetime
 
 from chart_generate import topn_requests_donut, yearoveryear_reqeusts_volume, delete_directory
 from data_fetch import data as dframe
 from tweet_generate import api, tweet
 
+
+## create directory to store program logs
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+    
+    
 timestamp = datetime.now().strftime('%A %B %d,%Y   %I:%M%p')
 module = sys.modules[__name__]
 pprint_module_name = ((str(module).split('from')[1]).replace('>',''))
@@ -15,9 +22,12 @@ image_files.append(yearoveryear_reqeusts_volume(dframe))
 for prd in ['year','week']:
     image_files.append(topn_requests_donut(dframe, period=prd))
 
+
 def run_program():
     tweet(api_object=api, files=image_files, msg=timestamp)
     delete_directory(image_folder)
+    shutil.move('execution.log',  os.path.join('logs','execution.log'))
+
     print('{} run sucessfully'.format(pprint_module_name))
     
     
